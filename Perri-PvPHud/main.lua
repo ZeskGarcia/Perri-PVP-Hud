08384ba7dca20aa3a910bbe644de1ad513642c83
TrolerTuber <94766847+TrolerTuber@users.noreply.github.com>
2024-06-17 04:30:58 +0200

    Delete Perri-PvPHud directory

--- Perri-PvPHud/main.lua
+++
@@ -1,7 +0,0 @@
-CreateThread(function()
-    while true do
-        local health = math.max(math.floor(GetEntityHealth(PlayerPedId()) - 100), 0)
-        SendNUIMessage({ action = "update", health = health })
-        Wait(300)
-    end
-end)
