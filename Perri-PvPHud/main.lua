7f38137a55b82cb7ecdbab934f1d6c8e2e069f1f
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2024-08-17 14:07:24 +0200

    Update main.lua
    improve code and compatibilize to the other pull requests

--- Perri-PvPHud/main.lua
+++ Perri-PvPHud/main.lua
@@ -1,7 +1,23 @@
+Config = {
+    DiscordInviteCode = "example"
+}
+
+function sendNui(action, data)
+    local payload = { type = tostring(action) }
+    if (data and type(data) == "table") then
+        for k,v in pairs(data) do
+            payload[k] = v;
+        end
+    end
+
+    SendNUIMessage(payload)
+end
+
 CreateThread(function()
-    while true do
+    sendNui('updateHud', { action = "inviteCode", value = Config.DiscordInviteCode })
+    while (true) do
         local health = math.max(math.floor(GetEntityHealth(PlayerPedId()) - 100), 0)
-        SendNUIMessage({ action = "update", health = health })
+        sendNui('updateHud', { action = "health", value = health })
         Wait(300)
     end
 end)
