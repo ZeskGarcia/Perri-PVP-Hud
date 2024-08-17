e7cdf2212ed062f5f030c225cbb4237309a01963
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2024-08-17 14:00:18 +0200

    Update app.js
    Add better code

--- Perri-PvPHud/nui/app.js
+++ Perri-PvPHud/nui/app.js
@@ -1,12 +1,18 @@
-
-window.addEventListener("message", (event) => {
-        let e = event.data;
-
-        if (e.action === "update") {
-            $("#hola").css({"width": Math.round(e.health) + "%", "top": 100 - Math.round(e.health) + "%"});
-            
-
-}})
-
-
-
+const app = new Vue({
+        el: '#app',
+        data: {
+                health: 0,
+                inviteCode: ""
+        },
+        methods: {
+                updateHud({ action, value }) {
+                        let avActions = ["health", "inviteCode"];
+                        let avVal = avActions.map(act => act === action);
+                        if (!avVal.includes(true)) return;
+                        this[action] = value;
+                }
+        },
+        mounted() {
+                window.addEventListener('message', async ({data}) => this[data.type] && this[data.type](data));
+        }
+);
